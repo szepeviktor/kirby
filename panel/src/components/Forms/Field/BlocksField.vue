183c8a261a56dca19e29194304fa8aa493cae2c6
Bastian Allgeier <mail@bastianallgeier.com>
2019-05-20 11:14:12 +0200

    New block component

--- panel/src/components/Forms/Field/BlocksField.vue
+++ panel/src/components/Forms/Field/BlocksField.vue
@@ -1,88 +1,86 @@
 <template>
   <k-field v-bind="$props" class="k-blocks-field" @click.native.stop>
 
-    <div class="k-block" data-type="headline">
-      <div class="k-block-info">
-        <span class="k-block-type">Heading 1</span>
-        <span class="k-block-preview">Hello world</span>
-      </div>
-      <k-button icon="dots" />
-    </div>
+    <!-- Add button -->
+    <template slot="options">
+      <k-button
+        ref="add"
+        icon="add"
+        @click="add"
+      >
+        {{ $t("add") }}
+      </k-button>
+    </template>
 
-    <div class="k-block" data-type="text">
-      <div class="k-block-info">
-        <span class="k-block-type">Text</span>
-        <span class="k-block-preview">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-        magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
-        elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-        accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet.</span>
-      </div>
-      <k-button icon="dots" />
-    </div>
+    <k-block type="heading" label="Heading 1">
+      <strong>Hello world</strong>
+    </k-block>
 
-    <div class="k-block" data-type="image">
-      <div class="k-block-info">
-        <span class="k-block-type">Image</span>
-        <span class="k-block-preview">
-          <span class="k-block-gallery">
-            <k-image src="http://sandbox.test/media/pages/sandbox/730881891-1556305734/aj-robbie-100x100.jpg" ratio="1/1" back="black" />
-          </span>
-        </span>
-      </div>
-      <k-button icon="dots" />
-    </div>
+    <k-block type="text" label="Text">
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+      magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+      gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+      elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+      accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet.
+    </k-block>
 
-    <div class="k-block" data-type="quote">
-      <div class="k-block-info">
-        <span class="k-block-type">Quote</span>
-        <span class="k-block-preview">
-          <p>If you love what you do you never have to work for a single day in your life</p>
-          <p>– Victor Hugo</p>
-        </span>
-      </div>
-      <k-button icon="dots" />
-    </div>
+    <k-block type="image" label="Image">
+      <span class="k-block-gallery">
+        <k-image
+          src="http://sandbox.test/media/pages/sandbox/730881891-1556305734/aj-robbie-100x100.jpg"
+          ratio="1/1"
+          back="black"
+        />
+      </span>
+    </k-block>
 
-    <div class="k-block" data-type="gallery">
-      <div class="k-block-info">
-        <span class="k-block-type">Gallery</span>
-        <span class="k-block-preview">
-          <span class="k-block-gallery">
-            <k-image src="http://sandbox.test/media/pages/sandbox/730881891-1556305734/aj-robbie-100x100.jpg" ratio="1/1" back="black" />
-            <k-image src="http://sandbox.test/media/pages/sandbox/730881891-1556305734/aj-robbie-100x100.jpg" ratio="1/1" back="black" />
-            <k-image src="http://sandbox.test/media/pages/sandbox/730881891-1556305734/aj-robbie-100x100.jpg" ratio="1/1" back="black" />
-          </span>
-        </span>
-      </div>
-      <k-button icon="dots" />
-    </div>
+    <k-block type="heading" label="Heading 2">
+      <strong>This is really nice</strong>
+    </k-block>
 
-    <div class="k-block" data-type="cta">
-      <div class="k-block-info">
-        <span class="k-block-type">CTA</span>
-        <span class="k-block-preview">Download now</span>
-      </div>
-      <k-button icon="dots" />
-    </div>
+    <k-block type="quote" label="Quote">
+      <p>If you love what you do you never have to work for a single day in your life</p>
+      <p>– Victor Hugo</p>
+    </k-block>
 
-    <div class="k-block" data-type="video">
-      <div class="k-block-info">
-        <span class="k-block-type">Video</span>
-        <span class="k-block-preview">https://youtube.com/watch?v=12dasj</span>
-      </div>
-      <k-button icon="dots" />
-    </div>
+    <k-block type="gallery" label="Gallery">
+      <span class="k-block-gallery">
+        <k-image src="http://sandbox.test/media/pages/sandbox/730881891-1556305734/aj-robbie-100x100.jpg" ratio="1/1" back="black" />
+        <k-image src="http://sandbox.test/media/pages/sandbox/730881891-1556305734/aj-robbie-100x100.jpg" ratio="1/1" back="black" />
+        <k-image src="http://sandbox.test/media/pages/sandbox/730881891-1556305734/aj-robbie-100x100.jpg" ratio="1/1" back="black" />
+      </span>
+    </k-block>
+
+    <k-block type="text" label="Text">
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+      magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+      gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+      elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos
+      et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet.
+    </k-block>
+
+    <k-block type="cta" label="CTA">
+      Download now
+    </k-block>
+
+    <k-block type="video" label="Video">
+      https://youtube.com/watch?v=12dasj
+    </k-block>
 
   </k-field>
 </template>
 
 <script>
 import Field from "../Field.vue";
+import Block from "./BlocksField/Block.vue";
 
 export default {
   inheritAttrs: false,
+  components: {
+    "k-block": Block
+  },
   props: {
     ...Field.props,
     blocks: Object,
@@ -95,39 +93,6 @@ export default {
 </script>
 
 <style lang="scss">
-
-.k-block {
-  min-height: 38px;
-  margin-bottom: 2px;
-  display: flex;
-  align-items: flex-start;
-  background: #fff;
-  box-shadow: $box-shadow-card;
-  padding: 0 .75rem;
-  font-size: .875rem;
-  justify-content: space-between;
-}
-.k-block .k-button {
-  height: 38px;
-  line-height: 1;
-}
-.k-block-info {
-  display: flex;
-  line-height: 1;
-  align-items: flex-start;
-}
-.k-block-type {
-  color: $color-light-grey;
-  margin-right: .75rem;
-  width: 5rem;
-  flex-shrink: 0;
-  padding: .5rem 0;
-  line-height: 1.5em;
-}
-.k-block-preview {
-  padding: .5rem 1.5rem .5rem 0;
-  line-height: 1.5em;
-}
 
 .k-block-gallery {
   display: flex;
---
+++ panel/src/components/Forms/Field/BlocksField/Block.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="k-block" :data-type="type">
+    <div class="k-block-info">
+      <span class="k-block-type">{{ label }}</span>
+      <span class="k-block-preview"><slot /></span>
+    </div>
+    <k-dropdown class="k-block-options">
+      <k-button icon="dots" @click="$refs.options.toggle()" />
+      <k-dropdown-content ref="options" align="right">
+        <k-dropdown-item icon="edit">Edit</k-dropdown-item>
+        <k-dropdown-item icon="add">Insert</k-dropdown-item>
+        <k-dropdown-item icon="trash" @click="$refs.delete.open()">Delete</k-dropdown-item>
+      </k-dropdown-content>
+    </k-dropdown>
+
+    <k-dialog ref="delete" theme="negative">
+      Do you really want to delete this block?
+    </k-dialog>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String
+    },
+    type: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.k-block {
+  min-height: 38px;
+  margin-bottom: 2px;
+  display: flex;
+  align-items: flex-start;
+  background: #fff;
+  box-shadow: $box-shadow-card;
+  padding: 0;
+  font-size: .875rem;
+  justify-content: space-between;
+}
+.k-block-options {
+  height: 38px;
+}
+.k-block-options > .k-button {
+  line-height: 1;
+  height: 100%;
+  padding: 0 .75rem;
+}
+.k-block-info {
+  display: flex;
+  line-height: 1;
+  align-items: flex-start;
+}
+.k-block-type {
+  color: $color-light-grey;
+  margin-right: .75rem;
+  width: 5rem;
+  flex-shrink: 0;
+  padding: .5rem 0 .5rem .75rem;
+  line-height: 1.5em;
+}
+.k-block-preview {
+  padding: .5rem 1.5rem .5rem 0;
+  line-height: 1.5em;
+}
+</style>
